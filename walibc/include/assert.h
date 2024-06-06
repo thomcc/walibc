d2c140ddbb453b254ac3be7bc335c160c77e8dfb
Thom Chiovoloni <thom@shift.click>
2024-06-06 13:35:46 -0700

    Add `static_assert` to assert.h

--- walibc/include/assert.h
+++ walibc/include/assert.h
@@ -7,4 +7,9 @@ __WALIBC_BEGIN_EXTERN;
 #else
 # define assert(X) ((void)((X) ? 0 : (__builtin_trap(), 0)))
 #endif
+
+#ifndef __cplusplus
+# define static_assert _Static_assert
+#endif
+
 __WALIBC_END_EXTERN;
