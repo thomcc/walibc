ef347f746a8b29dbec9ee30034813e4e081cf8ed
Thom Chiovoloni <thom@shift.click>
2024-06-06 07:29:39 -0700

    Align max_align_t with wasi

--- walibc/include/stddef.h
+++ walibc/include/stddef.h
@@ -5,7 +5,7 @@ __WALIBC_BEGIN_EXTERN;
 // Most of this is provided by `__walibc_ints.h`.
 
 // TODO: Is this right?
-typedef union { long long __Priv0; long double __Priv1; } max_align_t;
+typedef struct { long long __Priv0; long double __Priv1; } max_align_t;
 
 
 __WALIBC_END_EXTERN;
